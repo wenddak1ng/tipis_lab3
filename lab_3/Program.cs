5baa7ef36c99df51699d8024f156519d1678e694
wenddak1ng <49898464+wenddak1ng@users.noreply.github.com>
2019-04-23 14:25:46 +0700

    Add files via upload

---
+++ lab_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab_3 {
+  class MainClass {
+    static string selected;
+    static void Main(string[] args) {
+      Console.Title = "Лабораторная работа 3";
+      pulemet pul = new pulemet();
+      while (true) {
+        Console.WriteLine("Доступные действия: Выстрел / Ствол / Температура / Стоп / Остановить");
+        Console.Write("Для продолжения выберите действие: ");
+        selected = Console.ReadLine().ToLower();
+        if (selected == "выстрел") {
+          Console.Write("Введите количество выстрелов: ");
+          selected = Console.ReadLine();
+          pul.Shot(int.Parse(selected));
+        }
+        else if (selected == "стоп") break;
+        else if (selected == "температура") pul.CheckTemp();
+        else if (selected == "ствол") pul.ChangeSt();
+        else if (selected == "остановить") {
+          Console.Write("Введите время паузы (в секундах): ");
+          selected = Console.ReadLine();
+          pul.Pause(int.Parse(selected));
+        }
+      }
+    }
+  }
+}
---
+++ lab_3/pulemet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab_3 {
+  public class pulemet {
+    const int V = 1, maxTst = 90, Tokrsr = 23, nag1st = 2, Vosts = 1;
+    int Stoptime;
+    int tekTst = Tokrsr;
+    public bool Shot(int amount) {
+      if (Stoptime > DateTime.Now.Second) {
+        Console.WriteLine("Данное действие в данный момент невозможно. Время перезарядки {0} секунд", Stoptime - DateTime.Now.Second);
+        return true;
+      }
+      int i = 0;
+      if ((tekTst + (nag1st * amount)) > maxTst) {
+        i = amount - ((maxTst - tekTst) / nag1st);
+        amount = amount - i;
+      }
+      tekTst += nag1st * amount; //++ temperature
+      Stoptime = DateTime.Now.Second + (V * amount); //get tick
+      Console.WriteLine("Произведено {0} выстрелов", amount);
+      Console.WriteLine("Время паузы: {0} секунд", Stoptime - DateTime.Now.Second); 
+      Console.WriteLine("Текущая температура ствола {0}C", tekTst);
+      if (i > 0) {
+        Console.WriteLine("Не произведено {0} выстрелов из-за перегрева ствола", i);
+      }
+      return true;
+    }
+    public void CheckTemp() => Console.WriteLine("Текущая температура ствола {0}C", tekTst);
+    public void ChangeSt() {
+      tekTst = Tokrsr;
+      Console.WriteLine("Ствол сменён. Текущая температура ствола {0}C", tekTst);
+    }
+    public void Pause( int time ) { 
+      Stoptime = DateTime.Now.Second + (V * time);
+    }
+  }
+}
